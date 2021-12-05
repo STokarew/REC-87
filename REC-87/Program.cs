b3b3363b0b3b92ab58ffdfcd69ecd836b6ee8f4c
сергей токарев <sseregatokarew@gmail.com>
2021-12-05 15:51:29 +0700

    Добавьте файлы проекта.

---
+++ REC-87/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace REC_87
+{
+    class Program
+    {
+        static String IsPalindrome(string Str)
+        {
+            Str = System.Text.RegularExpressions.Regex.Replace(Str, @"[\.!,\s:;?]", "");
+            Str = Str.ToLower();
+            if (Str == new string(Str.Reverse().ToArray()))
+                return "полиндром.";
+            else
+                return "не полиндром.";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            Console.Write($"Введённая строка - {IsPalindrome(Console.ReadLine())}");
+            Console.ReadKey();
+        }
+    }
+}
